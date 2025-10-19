ecbbcbc206c6590751fa0ffffa43f6cbd6294330
Valentina <34088724+htmlcoderexe@users.noreply.github.com>
2025-10-19 04:37:20 +0200

    baddies get animated too now

--- src/gameObjects/Actor.js
+++ src/gameObjects/Actor.js
@@ -1,15 +1,29 @@
 class Actor extends GameObject
 {
+    basicshape ={};
+    animation = null;
     HP = 10;
     MaxHP = 10;
     constructor(type="actor")
     {
         super(type);
+        this.animation=VectorSprite.fromRawObject(this.basicshape);
+        console.log(this.animation);
     }
     update(dT)
     {
+        if(this.animation)
+            this.animation.animations['idle'].update(dT);
         if(this.HP<=0)
             this.die();
         super.update(dT);
+    }
+    draw(ctx)
+    {
+        
+        ctx.resetTransform();
+        ctx.translate(this.x,this.y);
+        this.animation.animations['idle'].draw(ctx);
+        ctx.resetTransform();
     }
 }
--- src/gameObjects/Hostile.js
+++ src/gameObjects/Hostile.js
@@ -1,15 +1,56 @@
 class Hostile extends Actor
 {
+    basicshape={
+        "idle": [
+            [
+                {
+                    "time": 0,
+                    "fill": "#000000",
+                    "path": "m 0,-25 c 25,0 45,20 45,30 c 0,10 -20,30 -45,30 c -25,0 -45,-20 -45,-30 c 0,-10 20,-30 45,-30 z"
+                },
+                {
+                    "time": 0.8,
+                    "fill": "#FF0F89",
+                    "path": "m 0,-21 c 15,0 40,25 40,32 c 0,7 -25,32 -40,32 c -15,0 -40,-25 -40,-32 c 0,-7 25,-32 40,-32 z"
+                },
+                {
+                    "time": 1.9,
+                    "fill": "#FF0F89",
+                    "path": "m 0,-25 c 25,0 45,20 45,30 c 0,10 -20,30 -45,30 c -25,0 -45,-20 -45,-30 c 0,-10 20,-30 45,-30 z"
+                }
+            ],
+            [
+                {
+                    "time": 0,
+                    "fill": "#000000",
+                    "path": "m 0,0 c 6,-0 6,15 0,25 c -6,0 -6,-15 0,-25 z"
+                },
+                {
+                    "time": 0.8,
+                    "fill": "#003050",
+                    "path": "m 0,5 c 6,-0 6,15 0,25 c -6,0 -6,-15 0,-25 z"
+                },
+                {
+                    "time": 1.9,
+                    "fill": "#003050",
+                    "path": "m 0,0 c 6,-0 6,15 0,25 c -6,0 -6,-15 0,-25 z"
+                }
+            ]
+        ]
+    };
     size = 50;
     constructor()
     {
-        super("hostile");
+        super("hostile"); 
+        this.animation=VectorSprite.fromRawObject(this.basicshape);
+       
         this.speed= 100;
         this.hitbox = new Rectangle(-25,-25,50,50);
         this.originalHitbox = new Rectangle(-25,-25,50,50);
     }
     draw(ctx)
     {
+        /*/
         let colr ="#FF00FF";
         if(this.HP<(this.MaxHP*0.9))
         {
@@ -21,6 +62,7 @@ class Hostile extends Actor
         }
         ctx.fillStyle = colr;
         ctx.fillRect(this.hitbox.x,this.hitbox.y,this.hitbox.width,this.hitbox.height);
+        //*/
         super.draw(ctx);
     }
     update(dT)
