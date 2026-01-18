09e01d3eacdb1b3088fe72c472f0744f5041c2fb
Valentina <34088724+htmlcoderexe@users.noreply.github.com>
2026-01-18 02:49:07 +0100

    added basic UI management

--- src/GUIManager.js
+++ src/GUIManager.js
@@ -1,4 +1,69 @@
 class GUIManager
 {
-    
+    components = [];
+    update(dT)
+    {
+
+    }
+    draw(ctx)
+    {
+        this.components.forEach((c)=>{
+            c.draw(ctx);
+        });
+    }
+    handlePrimaryPointerMove(e)
+    {
+
+    }
+    handleSecondaryPointerMove(e)
+    {
+
+    }
+    handlePrimaryPointerUp(e)
+    {
+
+    }
+    handleSecondaryPointerUp(e)
+    {
+
+    }
+    handlePrimaryPointerDown(e)
+    {
+
+    }
+    handleSecondaryPointerDown(e)
+    {
+
+    }
+    handlePrimaryPointerClick(e)
+    {
+
+    }
+    handleClick(e)
+    {
+        console.log(e);
+        let handled = false;
+        this.components.forEach((c)=>{
+            const target = c.checkhit(e.offsetX,e.offsetY);
+            if(target)
+            {
+                console.log("yeeee");
+                target.click();
+                handled = true;
+            }
+        });
+        return handled;
+    }
+    handleKeyDown(e)
+    {
+
+    }
+    handleKeyUp(e)
+    {
+
+    }
+    handleKeyPress(e)
+    {
+
+    }
 }
