d5f7cc273cc99d672856a0fffe48c5143a9d2633
htmlcoderexe <34088724+htmlcoderexe@users.noreply.github.com>
2026-02-17 17:53:33 +0100

    Inventory stub

---
+++ gameObjects/Inventory.js
@@ -0,0 +1,23 @@
+class Inventory 
+{
+    items = [];
+    overflow = [];
+    capacity = 1;
+    constructor(size)
+    {
+        this.capacity=size;
+        this.items = new Array(size);
+    }
+    checkFit(item)
+    {
+
+    }
+    add(item, overflow=false)
+    {
+
+    }
+    remove(item, quantity=0)
+    {
+
+    }
+}
