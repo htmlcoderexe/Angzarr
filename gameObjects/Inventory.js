feb400e70c7fc2ff68a1aa1de3a5dd67cacd5018
Valentina <34088724+htmlcoderexe@users.noreply.github.com>
2026-02-20 08:18:51 +0100

    File stubs necessary for inventory view

--- gameObjects/Inventory.js
+++ gameObjects/Inventory.js
@@ -14,10 +14,17 @@ class Inventory
     }
     add(item, overflow=false)
     {
-
+        // for now
+        this.items.push(item);
     }
     remove(item, quantity=0)
     {
 
+    }
+    *[Symbol.iterator]() {
+        for(const item of this.items)
+        {
+            yield item;
+        }
     }
 }
