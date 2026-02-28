d5fbeaf7118457b1177577b765128d06b3612f93
Valentina <34088724+htmlcoderexe@users.noreply.github.com>
2026-02-28 19:34:45 +0100

    Bugfix: debug hitboxes render correctly now

--- GameObject.js
+++ GameObject.js
@@ -248,7 +248,7 @@ class GameObject
         {
             ctx.strokeStyle="#FF0000";
             ctx.lineWidth=1;
-            ctx.strokeRect(this.hitbox.x+0.5,this.hitbox.y+0.5,this.hitbox.width,this.hitbox.height);
+            ctx.strokeRect(this.originalHitbox.x,this.originalHitbox.y,this.hitbox.width,this.hitbox.height);
         }
         ctx.translate(-this.x,-this.y);
     }
